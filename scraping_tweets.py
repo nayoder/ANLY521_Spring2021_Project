89a3ba43d65d4cf8724f60878d1b9b3e621f5ed7
nayoder <nicole.a.yoder@gmail.com>
2021-04-30 11:43:30 -0400

    Cleaning up some of the code and adding date_until argument

--- scraping_tweets.py
+++ scraping_tweets.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-import time
 from dotenv import load_dotenv
 import pandas as pd
 import tweepy
@@ -18,7 +17,7 @@ TWITTER_ACCESS_SECRET = os.getenv('TWITTER_ACCESS_SECRET')
 def create_query(filename):
     """
     Converts a text file of newline-separated Twitter search phrases into a
-    Tweepy API query separated by ORs.
+    Tweepy API query separated by "OR"s.
 
     Parameters
     ----------
@@ -28,7 +27,7 @@ def create_query(filename):
     Returns
     -------
     query : str
-        String in Tweepy query format (separated by ORs).
+        String in Tweepy query format (separated by "OR"s).
 
     """
     with open(filename, 'r') as file:
@@ -36,8 +35,8 @@ def create_query(filename):
             if i == 0:
                 query = line.strip()
             if i > 0:
-                query += f' OR {line.strip()}'
-    query += ' -filter:retweets'
+                query += f" OR {line.strip()}"
+    query += " -filter:retweets"
     return query
 
 
@@ -80,10 +79,10 @@ Hashtags Used:{ith_tweet["hashtext"]}
 
 
 
-def limit_handled(cursor):
+def yield_next(cursor):
     """
-    Yields the next tweet retrieved by the cursor, but handles the RateLimitError
-    and StopIteration at the end of the generator.
+    Yields the next tweet retrieved by the cursor, but handles the StopIteration
+    at the end of the generator and allows the user to interrupt with Ctrl-C.
 
     Parameters
     ----------
@@ -96,14 +95,15 @@ def limit_handled(cursor):
         tweet data
 
     """
-    while True:
-        try:
-            yield next(cursor)
-        except tweepy.RateLimitError:
-            print('Rate Limit Error, waiting 15 minutes')
-            time.sleep(15 * 60)
-        except StopIteration:
-            return
+    try:
+        while True:
+            try:
+                yield next(cursor)
+            except StopIteration:
+                return
+    except KeyboardInterrupt:
+        print("\nScraping tweets has been interrupted!")
+
 
 
 def clean_write(df, filename):
@@ -129,8 +129,8 @@ def clean_write(df, filename):
     print(df.info())
     
     # Certain columns need to be converted to int
-    cols = ["following", "followers", "totaltweets", "retweetcount"]
-    df[cols] = df[cols].astype("int")
+    cols = ['following', 'followers', 'totaltweets', 'retweetcount']
+    df[cols] = df[cols].astype('int')
     print('\n\nConverted int columns:')
     print(df.info())
     
@@ -139,7 +139,7 @@ def clean_write(df, filename):
     df.to_pickle(path)
 
 
-def scrape(phrases_filename, date_since, numtweet):
+def scrape(phrases_filename, date_since, date_until, numtweet):
     """
     Uses Tweepy API to search for tweets since a given date that use given
     particular phrases and then creates a csv file with the following data:
@@ -151,7 +151,11 @@ def scrape(phrases_filename, date_since, numtweet):
     phrases_filename : str
         Name of file that has search phrases/hashtags.
     date_since : str
-        Date in the form 'yyyy-mm-dd' to search since.
+        Date in the form 'yyyy-mm-dd' to search since (inclusive).
+    date_until : str
+        Date in the form 'yyyy-mm-dd' to search until (non-inclusive).
+        Ex: If date_since='2021-04-28' and date_until='2021-04-30', then the 
+        28th and the 29th will be included--not the 30th.
     numtweet : int
         Number of tweets to limit to if wanted.
 
@@ -173,17 +177,19 @@ def scrape(phrases_filename, date_since, numtweet):
     # We are using .Cursor() to search through twitter for the required tweets.
     # The number of tweets can be restricted using .items(number of tweets)
     if numtweet is None:
-        tweets = tweepy.Cursor(api.search, q=search_phrases, lang="en",
-                               since=date_since, tweet_mode='extended').items()
+        tweets = tweepy.Cursor(api.search, q=search_phrases, lang='en',
+                               since=date_since, until=date_until,
+                               tweet_mode='extended').items()
     else:
-        tweets = tweepy.Cursor(api.search, q=search_phrases, lang="en",
-                               since=date_since, tweet_mode='extended').items(numtweet)
+        tweets = tweepy.Cursor(api.search, q=search_phrases, lang='en',
+                               since=date_since, until=date_until,
+                               tweet_mode='extended').items(numtweet)
 
     # Counter to maintain Tweet Count
     i = 1
     
     # Iterate over each tweet in the list to extract information about each tweet
-    for tweet in limit_handled(tweets):
+    for tweet in yield_next(tweets):
         # Getting hashtags into a list
         hashtags = tweet.entities['hashtags']
         hashtext = list()
@@ -191,16 +197,16 @@ def scrape(phrases_filename, date_since, numtweet):
             hashtext.append(hashtags[j]['text'])
         
         tweet_data = {
-            "username" : tweet.user.screen_name,
-            "date" : tweet.created_at,
-            "description" : tweet.user.description,
-            "location" : tweet.user.location,
-            "following" : tweet.user.friends_count,
-            "followers" : tweet.user.followers_count,
-            "totaltweets" : tweet.user.statuses_count,
-            "retweetcount" : tweet.retweet_count,
-            "text" : tweet.full_text,
-            "hashtext" : hashtext
+            'username' : tweet.user.screen_name,
+            'date' : tweet.created_at,
+            'description' : tweet.user.description,
+            'location' : tweet.user.location,
+            'following' : tweet.user.friends_count,
+            'followers' : tweet.user.followers_count,
+            'totaltweets' : tweet.user.statuses_count,
+            'retweetcount' : tweet.retweet_count,
+            'text' : tweet.full_text,
+            'hashtext' : hashtext
         }
         
         # Here we are appending all the extracted information in the DataFrame
@@ -221,14 +227,17 @@ def scrape(phrases_filename, date_since, numtweet):
 if __name__ == '__main__':
     # Setting up arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--search_phrases_file", type=str,
-                        default="search_phrases.txt",
+    parser.add_argument('--search_phrases_file', type=str,
+                        default='search_phrases.txt',
                         help="Text file of the hashtags/phrases you want to search for")
-    parser.add_argument("--date_since", type=str,
-                        default="2021-04-28",
+    parser.add_argument('--date_since', type=str,
+                        default='2021-04-28',
                         help="Starting date for tweet search")
-    parser.add_argument("--num_tweets", type=int,
-                        default=None,############################################### possibly change
+    parser.add_argument('--date_until', type=str,
+                        default='2021-04-30',
+                        help="Starting date for tweet search")
+    parser.add_argument('--num_tweets', type=int,
+                        default=None,
                         help="Number of tweets to search for")
     args = parser.parse_args()
 
@@ -239,4 +248,4 @@ if __name__ == '__main__':
     auth.set_access_token(TWITTER_ACCESS_KEY, TWITTER_ACCESS_SECRET)
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     
-    scrape(args.search_phrases_file, args.date_since, args.num_tweets)
+    scrape(args.search_phrases_file, args.date_since, args.date_until, args.num_tweets)
